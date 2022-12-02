5a084676ba9a1c1fae216acedf4ea8d9bb1649b2
Eshant Sonune <eshantsonune52@gmail.com>
2022-12-03 02:26:14 +0530

    simulation update version 1

--- js/main.js
+++ js/main.js
@@ -1,71 +1,146 @@
-// Inverse of number using extended euclidean.
-function inv(div_r, div_t) {
-    var x = 1, y = 0, z, r, a = Number.parseInt(div_r), m = Number.parseInt(div_t);
+(function (window) {
+    // Variables
+    var dc = {};
+    let count = 0;
+    
+    let getHtmlFormInputSnippet = (count) => `
+        <div id="inputItem${count}" class="col-12 row">
+            <div class="col-1 py-2">
+                <div class="text-warning text-center">X</div>
+            </div>
+            <div class="col-1 py-2">
+                <div class="text-warning text-center">%</div>
+            </div>
+            <div class="col-3">
+                <label for="mod" class="visually-hidden">Modulie</label>
+                <input type="number" class="form-control h-75" id="mod${count}" placeholder="Modulie">
+            </div>
+            <div class="col-1 py-2">
+                <div class="text-warning text-center">=</div>
+            </div>
+            <div class="col-3">
+                <label for="rem" class="visually-hidden">Remainder</label>
+                <input type="number" class="form-control h-75" id="rem${count}" placeholder="Remainder">
+            </div>
+            <div class="col-3 pb-2">
+                <button onclick="$dc.addItem()" class="btn pt-0 float-end"><img width="35" src="./plus-circle.svg">
+                </button>
+            </div>
+        </div>
+    `;
 
-    while (true) {
-        if (m == 0) {
-            if (a != 1) {
-                console.log(`Reason: gcd ${div_r} and ${div_t} is not same`);
-                break;
+    // Inverse of number using extended euclidean.
+    function inv(div_r, div_t) {
+        let x = 1, y = 0, z, r, a = Number.parseInt(div_r), m = Number.parseInt(div_t);
+    
+        while (true) {
+            if (m == 0) {
+                if (a != 1) {
+                    crtError(`Relative prime error.`);
+                    break;
+                }
+                return (x < 0) ? (div_t + x) : x;
             }
-            return (x < 0) ? (div_t + x) : x;
+            r = Number.parseInt(a % m);
+            z = Number.parseInt(x - (a / m) * y);
+            a = m;
+            m = r;
+            x = y;
+            y = z;
         }
-        r = Number.parseInt(a % m);
-        z = Number.parseInt(x - (a / m) * y);
-        a = m;
-        m = r;
-        x = y;
-        y = z;
     }
-}
-
-// Chinese remainder theorem program.
-function getCRT(mod, rem, size) {
-    var M = 1, result = 0, t;
-
-    for (var i = 0; i < size; i++) {
-        M *= mod[i];
-    }
-    var temp;
-    for (var i = 0; i < size; i++) {
-        t = M / mod[i];
-        temp = (rem[i] * t * inv(t, mod[i]));
-        result += temp;
-        console.log(temp);
+    
+    // Chinese remainder theorem program.
+    function getCRT(mod, rem, size) {
+        var M = 1, result = 0, t;
+    
+        for (var i = 0; i < size; i++) {
+            M *= mod[i];
+        }
+        var temp;
+        for (var i = 0; i < size; i++) {
+            t = M / mod[i];
+            temp = (rem[i] * t * inv(t, mod[i]));
+            result += temp;
+            console.log(temp);
+        }
+    
+        result %= M;
+        return (result < 0) ? (M + result) : result;
     }
 
-    result %= M;
-    return (result < 0) ? (M + result) : result;
-}
+    let crtResult = (result) => {
+        $('#crt_result').addClass('mt-5');
+        $("#crt_result").html(
+            `<h3 class="text-warning text-center">
+                ${result}
+            </h3>`
+        );
+    };
 
-// // Driver method
+    let crtError = (error) => {
+        $('#crt_error').addClass('mt-5');
+        $('#crt_error').html(
+            `<h3 class="text-danger text-center">
+                ${error}
+            </h3>`
+        );
+    };
+
+    $(document).ready(function() {
+        dc.doCalc = () => {
+            crtError(``);
+            modArr = [];
+            remArr = [];
+            let x;
+            for (let i = 0; i <= count; i++) {
+                let mod = document.getElementById(`mod${i}`).value;
+                let rem = document.getElementById(`rem${i}`).value;
+                if (mod=='' || rem=='') {
+                    crtError(`Input values`);
+                    return;
+                }
+                modArr.push(parseInt(mod));
+                remArr.push(parseInt(rem));
+            }
+            console.log(modArr, remArr, count);
+            x = getCRT(modArr, remArr, modArr.length);
+            console.log(x);
+            crtResult(`The value of X is ${x}`);
+        };
+        
+        dc.addItem = () => {
+            window.event.preventDefault();
+            $(`#inputItem${count} :nth-child(6)`).html(
+                `<button onclick="$dc.deleteItem(${count})" class="btn pt-0 float-end"><img width="35" src="./minus-circle.svg">
+                </button>`
+            );
+            count++;
+            $("#sim-container").append(getHtmlFormInputSnippet(count));
+        };
+        
+        dc.deleteItem = (num) => {
+            let i;
+            count--;
+            $(`#inputItem${num}`).remove();
+            for (i=num; i<=count; i++) 
+            {
+                $(`#inputItem${i+1}`).attr("id", `inputItem${i}`);
+                $(`#inputItem${i} :nth-child(3) > input`).attr("id", `mod${i}`);
+                $(`#inputItem${i} :nth-child(5) > input`).attr("id", `rem${i}`);
+                if (i!=count) {
+                    $(`#inputItem${i} :nth-child(6) > button`).attr("onclick", `$dc.deleteItem(${i})`);
+                }    
+            }
+        };
+    });
+    
+
+    window.$dc = dc;
+})(window);
+
+
+// // Test 
 // var num = [3, 5, 7];
 // var rem = [2, 3, 2];
 // console.log("x is " + getCRT(num, rem, 3));
-var count = 0;
-const doCalc = () => {
-    numArr = [];
-    remArr = [];
-    for (let i = 0; i <= count; i++) {
-        let num = document.getElementById(`num${i}`).value;
-        let rem = document.getElementById(`rem${i}`).value;
-        numArr.push(parseInt(num));
-        remArr.push(parseInt(rem));
-    }
-    console.log(numArr, remArr, count);
-    let x = getCRT(numArr, remArr, numArr.length);
-    document.getElementById('result').innerHTML = `<br><h3>The value of X is :${x}</h3>`;
-};
-
-const addItem = () => {
-    let container = document.getElementById("sim-container");
-    window.event.preventDefault();
-    ++count;
-    container.innerHTML += `
-        <br>
-        <label for="fname">X % </label>
-        <input type="text" id="num${count}" name="num${count}" placeholder="Number" required>
-        <label for="lname">=</label>
-        <input type="text" id="rem${count}" name="rem${count}" placeholder="Remainder">
-        <button onclick="addItem()" class="add"><img width="35" src="./plus.png"></button>`;
-};
---
+++ js/test.js
@@ -0,0 +1,5 @@
+let message = "Hello! I am Eshant Sonune.";
+
+(function (para) {
+    console.log(para);
+})(message);
